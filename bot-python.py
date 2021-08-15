2e335b18df6bff4d762d549b859b3e843ad1a1c7
nobody48sheldor <lolmyizi@gmail.com>
2021-08-15 16:23:56 +0200

    some update, including js

--- bot-python.py
+++ bot-python.py
@@ -1,5 +1,6 @@
 import discord
 import os
+from sys import platform
 
 client = discord.Client()
 
@@ -7,6 +8,17 @@ os.chdir('.env')
 with open('token.txt', 'r') as tok:
     token = tok.read()
 os.chdir('..')
+
+def run(link):
+    os.chdir('github_folder')
+    print(os.name)
+    os.system('git clone {}'.format(link))
+    if platform == 'linux' or platform == "linux2":
+        print('del link linux')
+    if os.name == 'win32':
+        print('del link windows')
+    os.chdir('..')
+
 
 @client.event
 async def on_ready():
@@ -16,9 +28,15 @@ async def on_ready():
 async def on_message(msg):
     if msg.author == client.user:
         return
+    if msg.content.startswith('£help'):
+        await msg.channel.send('help pannel in working...')
     if msg.content.startswith('£hi'):
         await msg.channel.send('hi!')
     if msg.content.startswith('£github'):
         await msg.channel.send('https://github.com/nobody48sheldor/Discord-Bot-multifunction')
+    if msg.content.startswith('£run'):
+        link = msg.content.lstrip('£run')
+        await msg.channel.send(link)
+        run(link)
 
 client.run(token)
